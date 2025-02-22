dd4639285e8d457a5378389748d11d6d8e908aa2
Bahadır Verir <150124846+bahadirverir@users.noreply.github.com>
2025-02-22 20:11:26 +0300

    Script updated

--- DatabaseScript.sql
+++ DatabaseScript.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA `TeknolojiFirmaDB` ;
+CREATE SCHEMA `FirmaDB` ;
 
-CREATE TABLE `TeknolojiFirmaDB`.`Çalışanlar` (
+CREATE TABLE `FirmaDB`.`Çalışanlar` (
   `ÇalışanID` INT NOT NULL AUTO_INCREMENT,
   `Ad` VARCHAR(45) NOT NULL,
   `Soyad` VARCHAR(45) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE `TeknolojiFirmaDB`.`Çalışanlar` (
   `Deneyim` INT NOT NULL,
   PRIMARY KEY (`ÇalışanID`));
   
-CREATE TABLE `TeknolojiFirmaDB`.`Projeler` (
+CREATE TABLE `FirmaDB`.`Projeler` (
   `ProjeID` INT NOT NULL,
   `Proje Adı` VARCHAR(45) NOT NULL,
   `Başlangıç Tarihi` VARCHAR(45) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE `TeknolojiFirmaDB`.`Projeler` (
   `Bütçe` INT NOT NULL,
   PRIMARY KEY (`ProjeID`));
   
-  CREATE TABLE `TeknolojiFirmaDB`.`Ofisler` (
+  CREATE TABLE `FirmaDB`.`Ofisler` (
   `OfisID` INT NOT NULL,
   `Lokasyon` VARCHAR(45) NOT NULL,
   `Ofis Durumu` VARCHAR(45) NOT NULL,
@@ -32,13 +32,13 @@ CREATE TABLE `TeknolojiFirmaDB`.`Projeler` (
   `İletişim` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`OfisID`));
 
-CREATE TABLE `TeknolojiFirmaDB`.`Departmanlar` (
+CREATE TABLE `FirmaDB`.`Departmanlar` (
   `DepartmanID` INT NOT NULL,
   `Departman Adı` VARCHAR(45) NOT NULL,
   `Çalışan Sayısı` INT NOT NULL,
   PRIMARY KEY (`DepartmanID`));
   
-  CREATE TABLE `TeknolojiFirmaDB`.`Müşteriler` (
+  CREATE TABLE `FirmaDB`.`Müşteriler` (
   `MüşteriID` INT NOT NULL,
   `Müşteri Adı` VARCHAR(45) NOT NULL,
   `Müşteri Soyadı` VARCHAR(45) NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE `TeknolojiFirmaDB`.`Departmanlar` (
   `Kart Tipi` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`MüşteriID`));
   
-  CREATE TABLE `TeknolojiFirmaDB`.`Siparişler` (
+  CREATE TABLE `FirmaDB`.`Siparişler` (
   `SiparişID` INT NOT NULL,
   `ÜrünID` INT NOT NULL,
   `MüşteriID` INT NOT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE `TeknolojiFirmaDB`.`Departmanlar` (
   `Sipariş Durumu` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`SiparişID`));
 
-CREATE TABLE `TeknolojiFirmaDB`.`Ürünler` (
+CREATE TABLE `FirmaDB`.`Ürünler` (
   `ÜrünID` INT NOT NULL,
   `Ürün Adı` VARCHAR(45) NOT NULL,
   `Garanti Süresi` INT NOT NULL,
@@ -67,64 +67,64 @@ CREATE TABLE `TeknolojiFirmaDB`.`Ürünler` (
   `Satılan Miktar` INT NOT NULL,
   PRIMARY KEY (`ÜrünID`));
   
-ALTER TABLE `TeknolojiFirmaDB`.`Çalışanlar` 
+ALTER TABLE `FirmaDB`.`Çalışanlar` 
 ADD COLUMN `DepartmanID` INT NOT NULL AFTER `Deneyim`,
 ADD COLUMN `OfisID` INT NOT NULL AFTER `DepartmanID`,
 ADD COLUMN `ProjeID` INT NOT NULL AFTER `OfisID`;
 
-ALTER TABLE `TeknolojiFirmaDB`.`Çalışanlar` 
+ALTER TABLE `FirmaDB`.`Çalışanlar` 
 MODIFY COLUMN `ProjeID` INT NULL;
 
-ALTER TABLE `TeknolojiFirmaDB`.`Çalışanlar` 
+ALTER TABLE `FirmaDB`.`Çalışanlar` 
 ADD CONSTRAINT `FK_Çalışanlar_Departman`
   FOREIGN KEY (`DepartmanID`)
-  REFERENCES `TeknolojiFirmaDB`.`Departmanlar` (`DepartmanID`)
+  REFERENCES `FirmaDB`.`Departmanlar` (`DepartmanID`)
   ON DELETE CASCADE
   ON UPDATE CASCADE,
 ADD CONSTRAINT `FK_Çalışanlar_Ofis`
   FOREIGN KEY (`OfisID`)
-  REFERENCES `TeknolojiFirmaDB`.`Ofisler` (`OfisID`)
+  REFERENCES `FirmaDB`.`Ofisler` (`OfisID`)
   ON DELETE CASCADE
   ON UPDATE CASCADE,
 ADD CONSTRAINT `FK_Çalışanlar_Proje`
   FOREIGN KEY (`ProjeID`)
-  REFERENCES `TeknolojiFirmaDB`.`Projeler` (`ProjeID`)
+  REFERENCES `FirmaDB`.`Projeler` (`ProjeID`)
   ON DELETE SET NULL
   ON UPDATE CASCADE;
 
-  ALTER TABLE `TeknolojiFirmaDB`.`Siparişler` 
+  ALTER TABLE `FirmaDB`.`Siparişler` 
   ADD CONSTRAINT `FK_Siparişler_Müşteriler`
   FOREIGN KEY (`MüşteriID`)
-  REFERENCES `TeknolojiFirmaDB`.`Müşteriler` (`MüşteriID`)
+  REFERENCES `FirmaDB`.`Müşteriler` (`MüşteriID`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
   
-  CREATE TABLE `TeknolojiFirmaDB`.`Müşteri-Ürün` (
+  CREATE TABLE `FirmaDB`.`Müşteri-Ürün` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `MüşteriID` INT NOT NULL,
   `ÜrünID` INT NOT NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `FK_Müşteri` FOREIGN KEY (`MüşteriID`) 
-  REFERENCES `TeknolojiFirmaDB`.`Müşteriler`(`MüşteriID`) 
+  REFERENCES `FirmaDB`.`Müşteriler`(`MüşteriID`) 
   ON DELETE CASCADE 
   ON UPDATE CASCADE,
   CONSTRAINT `FK_Ürün` FOREIGN KEY (`ÜrünID`) 
-  REFERENCES `TeknolojiFirmaDB`.`Ürünler`(`ÜrünID`) 
+  REFERENCES `FirmaDB`.`Ürünler`(`ÜrünID`) 
   ON DELETE CASCADE 
   ON UPDATE CASCADE);
 
-CREATE TABLE `TeknolojiFirmaDB`.`Departman-Müşteri` (
+CREATE TABLE `FirmaDB`.`Departman-Müşteri` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `DepartmanID` INT NOT NULL,
   `MüşteriID` INT NOT NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `FK_Departman-Müşteri_DepartmanID`
     FOREIGN KEY (`DepartmanID`)
-    REFERENCES `TeknolojiFirmaDB`.`Departmanlar` (`DepartmanID`)
+    REFERENCES `FirmaDB`.`Departmanlar` (`DepartmanID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `FK_Departman-Müşteri_MüşteriID`
     FOREIGN KEY (`MüşteriID`)
-    REFERENCES `TeknolojiFirmaDB`.`Müşteriler` (`MüşteriID`)
+    REFERENCES `FirmaDB`.`Müşteriler` (`MüşteriID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
